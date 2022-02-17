5c47e225ed29342dd39fe8ecdf5e06417442fec0
Snow Lukin <everyoursden@gmail.com>
2022-02-18 00:19:48 +0300

    Initial Commit

---
+++ Colorized/ColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp.swift
+//  Colorized
+//
+//  Created by Snow Lukin on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
